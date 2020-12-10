6d2e78104aa4d72ff222a3fab2cc411b5dada63c
linda_gutierrez <linda.gutierrez@uruit.com>
2020-12-10 16:55:28 -0500

    messages

--- middlewares/error.js
+++ middlewares/error.js
@@ -7,14 +7,17 @@ const errorResponseFormatter = (err, req, res, next) => {
   if (process.env.NODE_ENV !== 'local') {
     return res.status(err.statusCode || 500).send({
       message: err.message,
+      reaction: err.face,
       status: 0,
     });
   }
 
   return res.status(err.statusCode || 500).send({
     message: `${err.message} \n ${err.stack}`,
+    reaction: err.face,
     status: 0,
   });
 };
 
 module.exports = { errorHandler, errorResponseFormatter };
+
--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,13 @@ const CustomError = require('../classes/error')
 
 router.get('/error',
   errorHandler(async (req, res) => {
-    // Let's simulate an error here for you to see how error are captured and formatted
+    // Let's simulate an error here for you to see how 
+    // errors are captured and formatted
     // without using any TRY CATCH
     throw new CustomError(500, 
-        'Este es un error capturado por nuestro super middleware errorHandler y formateado por nuestro otro super middleware errorResponseFormatter', 
+        'Este es un error capturado por nuestro super \
+        middleware errorHandler y formateado por nuestro  \
+        otro super middleware errorResponseFormatter', 
         ':)'
         )
   }));
@@ -17,9 +20,16 @@ router.get('/error',
 
 router.get('/success',
   errorHandler(async (req, res) => {
-    // This is a good response in case you want to see a successful case response
-    // This could be a async call, for example a call to our controllers that are connected to a database or a service, that might throw errors
-    const response = [{name: "Elsa Capunta", age: 40}, {name: "Alan Brito", age: 22}, {name: "Alba Bosa", age: 31}]
+    // This is a good response in case you want 
+    // to see a successful case response
+    // This could be a async call, for example a call to 
+    // our controllers that are connected to a database
+    // or a service, that might throw errors
+    const response = [
+      {name: "Elsa Capunta", age: 40}, 
+      {name: "Alan Brito", age: 22}, 
+      {name: "Alba Bosa", age: 31}
+    ]
     res.send(response)
 }));
 
